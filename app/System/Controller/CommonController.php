73ea683d2d6fe561281555520f7f58b661c72551
X.Mo <root@imoi.cn>
2023-01-08 20:03:03 +0800

    up

--- app/System/Controller/CommonController.php
+++ app/System/Controller/CommonController.php
@@ -155,4 +155,72 @@ class CommonController extends MineController
         $this->userService->clearCache((string) user()->getId());
         return $this->success();
     }
+
+    #[GetMapping("getOk")]
+    public function getOk(): ResponseInterface
+    {
+        $key = $this->request->input('key');
+        if ($key == '1') {
+            return $this->success([
+                ['label' => 'AA1', 'value' => 'AA1'],
+                ['label' => 'BB1', 'value' => 'BB1'],
+                ['label' => 'CC1', 'value' => 'CC1'],
+                ['label' => 'DD1', 'value' => 'DD1'],
+                ['label' => 'EE1', 'value' => 'EE1'],
+            ]);
+        }
+
+        if ($key == '2') {
+            return $this->success([
+                ['label' => 'AA2', 'value' => 'AA2'],
+                ['label' => 'BB2', 'value' => 'BB2'],
+                ['label' => 'CC2', 'value' => 'CC2'],
+                ['label' => 'DD2', 'value' => 'DD2'],
+                ['label' => 'EE2', 'value' => 'EE2'],
+            ]);
+        }
+
+        return $this->success([]);
+    }
+
+    #[GetMapping("getOk2")]
+    public function getOk2(): ResponseInterface
+    {
+        $key = $this->request->input('key');
+        if ($key == 'AA1') {
+            return $this->success([
+                ['label' => 'AA1', 'value' => 'AA1'],
+                ['label' => 'AB1', 'value' => 'AB1'],
+                ['label' => 'AC1', 'value' => 'AC1'],
+                ['label' => 'AD1', 'value' => 'AD1'],
+                ['label' => 'AE1', 'value' => 'AE1'],
+            ]);
+        }
+        if ($key == 'BB1') {
+            return $this->success([
+                ['label' => 'BA1', 'value' => 'BA1'],
+                ['label' => 'BB1', 'value' => 'BB1'],
+                ['label' => 'BC1', 'value' => 'BC1'],
+                ['label' => 'BD1', 'value' => 'BD1'],
+                ['label' => 'BE1', 'value' => 'BE1'],
+            ]);
+        }
+
+        if ($key == 'CC1') {
+            return $this->success([
+                ['label' => 'CA1', 'value' => 'CA1'],
+                ['label' => 'CB1', 'value' => 'CB1'],
+                ['label' => 'CC1', 'value' => 'CC1'],
+                ['label' => 'CD1', 'value' => 'CD1'],
+                ['label' => 'CE1', 'value' => 'CE1'],
+            ]);
+        }
+        return $this->success([
+            ['label' => 'ZA1', 'value' => 'ZA1'],
+            ['label' => 'ZB1', 'value' => 'ZB1'],
+            ['label' => 'ZC1', 'value' => 'ZC1'],
+            ['label' => 'ZD1', 'value' => 'ZD1'],
+            ['label' => 'ZE1', 'value' => 'ZE1'],
+        ]);
+    }
 }
